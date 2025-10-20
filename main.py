24f9552c5e0d7b42470d8cc762bfbf6670d32c43
svillors <tntzator9@gmail.com>
2025-10-20 20:37:10 +0200

    Handle ClienError

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import aiohttp
 import asyncio
+from enum import Enum
+
+import aiohttp
 import anyio
 import pymorphy2
-from adapters import SANITIZERS
 
+from adapters import SANITIZERS
 from text_tools import split_by_words, calculate_jaundice_rate
 
 
-TEST_ARTICLES = ['https://inosmi.ru/20251013/tomahawk-275135914.html', 'https://inosmi.ru/20251013/gaza-275134570.html', 'https://inosmi.ru/20251020/sholts-275262095.html']
+TEST_ARTICLES = ['https://inosmi.ru/202123123йцуйцуйцуйцу51013/tomahawk-275135914.html', 'https://inosmi.ru/20251013/gaza-275134570.html', 'https://inosmi.ru/20251020/sholts-275262095.html']
 sanitize = SANITIZERS.get('inosmi_ru')
+
+
+class ProcessingStatus(Enum):
+    OK = 'OK'
+    FETCH_ERROR = 'FETCH_ERROR'
 
 
 def fetch_charged_words(path, morph):
@@ -18,18 +25,29 @@ def fetch_charged_words(path, morph):
 
 
 async def fetch(session, url):
-    async with session.get(url) as response:
-        response.raise_for_status()
-        return await response.text()
+    try:
+        async with session.get(url) as response:
+            response.raise_for_status()
+            return await response.text()
+    except aiohttp.ClientError:
+        return
 
 
 async def process_article(session, morph, charged_words, url, results):
     html = await fetch(session, url)
+
+    if not html:
+        results.append((url, None, None, ProcessingStatus.FETCH_ERROR.value))
+        return
+
     text = sanitize(html, plaintext=True)
     splited_text = split_by_words(morph, text)
     jaundice_rate = calculate_jaundice_rate(splited_text, charged_words)
 
-    results.append((url, jaundice_rate, len(splited_text)))
+    results.append((
+        url, jaundice_rate,
+        len(splited_text), ProcessingStatus.OK.value
+    ))
 
 
 async def main():
@@ -47,8 +65,9 @@ async def main():
                     url, results
                 )
 
-        for url, rate, count in results:
-            print(f'URL: {url}\nРейтинг: {rate}\nКол-во слов: {count}\n')
+        for url, rate, count, status in results:
+            print(f'URL: {url}\nСтатус: {status}\n' \
+                  f'Рейтинг: {rate}\nКол-во слов: {count}\n')
 
 
 if __name__ == "__main__":
