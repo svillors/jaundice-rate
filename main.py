bb01ea81e4754157f75fddd0506d29a4408f8199
svillors <tntzator9@gmail.com>
2025-10-20 19:46:45 +0200

    Add charged words

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from adapters import SANITIZERS
 from text_tools import split_by_words, calculate_jaundice_rate
 
 
+def fetch_charged_words(path, morph):
+    with open(path) as f:
+        splited_text = split_by_words(morph, f.read())
+        return splited_text
+
+
 async def fetch(session, url):
     async with session.get(url) as response:
         response.raise_for_status()
@@ -14,12 +20,16 @@ async def fetch(session, url):
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'https://inosmi.ru/20251020/sholts-275262095.html')
         inosmi_sanitizer = SANITIZERS.get('inosmi_ru')
-        text = inosmi_sanitizer(html, plaintext=True)
         morph = pymorphy2.MorphAnalyzer()
+
+        html = await fetch(session, 'https://inosmi.ru/20251020/sholts-275262095.html')
+        text = inosmi_sanitizer(html, plaintext=True)
+
+        charged_words = fetch_charged_words(
+            './charged_dict/negative_words.txt', morph)
         splited_text = split_by_words(morph, text)
-        jaundice_rate = calculate_jaundice_rate(splited_text, ['Америка', 'шольц', 'трамп'])
+        jaundice_rate = calculate_jaundice_rate(splited_text, charged_words)
         print(f'Рейтинг: {jaundice_rate}\nСлов в статье: {len(splited_text)}')
 
 
