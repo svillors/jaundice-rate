e984a4f4a552c230dfab574921593ef9548e2633
svillors <tntzator9@gmail.com>
2025-10-21 20:24:44 +0200

    create dict to response

--- server.py
+++ server.py
@@ -6,7 +6,18 @@ from main import analyze_urls
 async def handle(request):
     urls = request.query.get('urls').split(',')
     analuzed_results = await analyze_urls(urls)
-    return web.json_response(analuzed_results)
+    response = []
+
+    for url, rate, count, status, time in analuzed_results:
+        result = {}
+        result['status'] = status
+        result['url'] = url
+        result['score'] = rate
+        result['words_count'] = count
+        result['analyze_time'] = time
+        response.append(result)
+
+    return web.json_response(response)
 
 
 app = web.Application()
